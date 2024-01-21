6c7a54b28affb8be1d137e98a3866a4897fe6baf
Gustavodeoliveiraa <ggu3131@gmail.com>
2024-01-21 02:35:30 -0300

    gerando bios automativa v1

--- cars/signals.py
+++ cars/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 from .models import Car, CarInventory
 from django.db.models import Sum
@@ -14,3 +14,9 @@ def car_post_save(sender, instance, **kwargs):
         cars_count=cars_count,
         cars_value=cars_value,
     )
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        instance.bio = 'Bio gerada automaticamente'
